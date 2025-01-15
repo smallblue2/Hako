9ba0dcca936d3d688100fbbced1468b2f75abb44
Niall Ryan <niallcryan@live.ie>
2025-01-15 18:17:04 +0000

    HO-38 Implemented 8 tests

--- src/tests/filesystem.test.js
+++ src/tests/filesystem.test.js
@@ -26,6 +26,13 @@ describe("Filesystem tests", () => {
   let browser;
   let page;
 
+  let initFS = async (pg) => {
+    pg.evaluate(async () => {
+      window.Filesystem.initialiseFS();
+      await (async (resolve) => { setTimeout(resolve, 3000) })(); // Empirically 3 seconds chosen to allow filesystem to initialise
+    })
+  };
+
   before(async () => {
     // Incognito launch as these tests are ran in Parallel
     // and we need IndexedDB instances to be completely
@@ -57,11 +64,120 @@ describe("Filesystem tests", () => {
     await page.close();
   });
 
-  it("checks whether Filesystem object exists on the window object", async () => {
+  it("Check whether Filesystem object exists on the window object", async () => {
     const hasFilesystem = await page.evaluate(() => {
       return typeof window.Filesystem !== "undefined";
     });
 
     assert.strictEqual(hasFilesystem, true, "Filesystem should exist on the window object");
   });
+
+  it("Check valid initialisation of the filesystem", async () => {
+
+    await initFS(page);
+
+    let persistentVolumeExists = await page.evaluate(async () => {
+      return window.Filesystem.access("persistent", window.Filesystem.F_OK) == 0;
+    });
+
+    assert.ok(persistentVolumeExists);
+  });
+
+  it("Confirm lack of persistence due to incognito tab", async () => {
+    const persistentVolumeExists = await page.evaluate(async () => {
+      return window.Filesystem.access("persistent", window.Filesystem.F_OK) == -1;
+    })
+
+    assert.ok(persistentVolumeExists);
+  });
+
+  it("Test `chdir`", async () => {
+
+    await initFS(page);
+
+    let origStat = await page.evaluate(async () => {
+      return window.Filesystem.stat("persistent")
+    });
+
+    let chdirRes = await page.evaluate(async () => {
+      return window.Filesystem.chdir("persistent") == 0;
+    });
+
+    let newStat = await page.evaluate(async () => {
+      return window.Filesystem.stat(".");
+    });
+
+    assert.ok(chdirRes);
+    assert.deepStrictEqual(origStat, newStat, "Stats are not equal, suggesting an incorrect chdir.");
+  })
+
+  it("Test creating a file with `open` and closing it with `close`", async () => {
+
+    await initFS(page);
+
+    let { notExistBefore, fd, closeRes, accessRes } = await page.evaluate(async () => {
+      let notExistBefore = window.Filesystem.access("persistent/hello.txt", window.Filesystem.F_OK) == -1;
+      let fd = window.Filesystem.open("persistent/hello.txt", window.Filesystem.O_CREAT, 0o333);
+      let closeRes = window.Filesystem.close(fd) == 0;
+      let accessRes = window.Filesystem.access("persistent/hello.txt", window.Filesystem.F_OK) == 0;
+      return { notExistBefore, fd, closeRes, accessRes }
+    });
+
+    assert.ok(notExistBefore, "File existed before test when it shouldn't");
+    assert.ok(fd >= 0, "Invalid file descriptor");
+    assert.ok(closeRes, "Couldn't close file descriptor");
+    assert.ok(accessRes, "File access failed after its creation");
+
+  })
+
+  it("Test `mkdir`", async () => {
+
+    await initFS(page);
+
+    let { accessResp, mkdirResp } = await page.evaluate(async () => {
+      let mkdirResp = window.Filesystem.mkdir("persistent/testdir") == 0;
+      let accessResp = window.Filesystem.access("persistent/testdir", window.Filesystem.F_OK) == 0;
+      return { accessResp, mkdirResp }
+    });
+
+    assert.ok(mkdirResp, "Failed to create directory");
+    assert.ok(accessResp, "Seems like directory doesn't exist");
+
+  });
+
+  it("Test `rmdir`", async () => {
+
+    await initFS(page);
+
+    let { mkdirResp, confirmCreat, rmdirResp, confirmDel } = await page.evaluate(async () => {
+      let mkdirResp = window.Filesystem.mkdir("persistent/testdir") == 0;
+      let confirmCreat = window.Filesystem.access("persistent/testdir", window.Filesystem.F_OK) == 0;
+      let rmdirResp = window.Filesystem.rmdir("persistent/testdir") == 0;
+      let confirmDel = window.Filesystem.access("persistent/testdir", window.Filesystem.F_OK) == -1;
+      return { mkdirResp, confirmCreat, rmdirResp, confirmDel }
+    })
+
+    assert.ok(mkdirResp, "Failed to create directory to delete");
+    assert.ok(confirmCreat, "Failed to confirm the creation of directory to delete");
+    assert.ok(rmdirResp, "Failed to remove directory");
+    assert.ok(confirmDel, "Directory still exists despite deletion");
+
+  })
+
+  it("Test `opendir`, `readdir` and `closedir`", async () => {
+
+    await initFS(page);
+
+    let { dd, readRes, closeRes } = await page.evaluate(async () => {
+      let dd = window.Filesystem.opendir(".");
+      let readRes = window.Filesystem.readdir(dd);
+      let closeRes = window.Filesystem.closedir(dd) == 0;
+      return { dd, readRes, closeRes }
+    });
+
+    assert.ok(dd >= 0, "Invalid directory descriptor when opening");
+    assert.ok(readRes.length > 0, "Opened directory was empty, expected non-empty");
+    assert.ok(closeRes, "Failed to close directory");
+    
+  })
 });
