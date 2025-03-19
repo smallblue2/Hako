4dab1a25649ddadbf1b07aa8a81017cde4baa3f5
ogradc23 <cathal.ogrady23@mail.dcu.ie>
2025-03-19 12:54:25 +0000

    HO-53-text-editing add inotify events to js filesystem api

---
+++ src/filesystem/post.js
@@ -0,0 +1,7 @@
+const syncfsOld = IDBFS.syncfs;
+const inotifyChannel = new BroadcastChannel("inotify");
+
+IDBFS.syncfs = (mount, populate, callback) => {
+  inotifyChannel.postMessage({});
+  return syncfsOld(mount, populate, callback);
+}
