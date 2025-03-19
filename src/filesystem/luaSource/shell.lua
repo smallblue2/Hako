cbd37912be9693c9c3b70f862fbf4cb2f4438e2d
ogradc23 <cathal.ogrady23@mail.dcu.ie>
2025-03-19 20:10:24 +0000

    HO-66-move-to-proxyfs-on-workers runtime now depends on IDBFS setup externally and mounts it through a PROXYFS

--- src/filesystem/luaSource/shell.lua
+++ src/filesystem/luaSource/shell.lua
@@ -1,2 +1,16 @@
 output("cathal smells")
 output(inspect(process.argv))
+local sr, err = file.stat("/persistent/sys/")
+if err ~= nil then
+  output(errors.as_string(err))
+  process.exit(1)
+end
+output(inspect(sr))
+sr, err = file.stat("/persistent/sys/shell.lua")
+if err ~= nil then
+  output(errors.as_string(err))
+  process.exit(1)
+end
+output(inspect(sr))
+-- output(inspect(select(1, file.stat("/persistent/sys/"))))
+-- output(inspect(select(1, file.stat("/persistent/sys/shell.lua"))))
