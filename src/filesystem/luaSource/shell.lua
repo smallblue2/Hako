2f7e853712e88d741fa78c4d96002ffc7553e585
ogradc23 <cathal.ogrady23@mail.dcu.ie>
2025-03-21 19:54:19 +0000

    HO-71 added ldoc stubs for LSP support and autodoc gen

--- src/filesystem/luaSource/shell.lua
+++ src/filesystem/luaSource/shell.lua
@@ -1,8 +1,9 @@
 local pid, err = process.create("/persistent/sys/hello.lua", { argv = {"--help"} })
 if err ~= nil then
-  output(errors.as_string())
+  output(errors.as_string(err))
   return
 end
+assert(pid ~= nil)
 process.start(pid)
 output("Process started, now waiting ...")
 process.wait(pid)
