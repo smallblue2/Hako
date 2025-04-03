d3bb9e132cf904a3b5f6d8a3ad3f4f06cda0c787
Niall Ryan <niallcryan@live.ie>
2025-04-04 00:35:50 +0100

    HO-89 Added directory handling
    Cat now errors if you pass a directory as an argument.

--- src/filesystem/luaSource/cat.lua
+++ src/filesystem/luaSource/cat.lua
@@ -9,18 +9,33 @@ end
 for i = 2, #process.argv do
 
   local filename = process.argv[i]
-  -- Open it
-  local fd, err0 = file.open(filename, "r")
-  -- Make sure it opened OK
+  -- Make sure it's a file and not a directory
+  local stat, err0 = file.stat(filename)
   if err0 then
-    output(string.format("Cat cannot open '%s' (err: %s)", filename, err0))
+    output(string.format("Erorr: Cannot stat '%s' (err: %s)", filename, err0))
+    return
+  end
+  if stat.type == DIRECTORY then
+    output(string.format("Error: '%s' is a directory", filename))
+    return
+  end
+
+  -- Open it
+  local fd, err1 = file.open(filename, "r")
+  -- Make sure it opened OK
+  if err1 then
+    output(string.format("Error: Cannot open '%s' (err: %s)", filename, err1))
   else
     -- Keep reading until we break the loop!
     while true do
       -- Try and read 1024 bytes
-      local chunk, err1 = file.read(fd, 1024)
-      if err1 then
-        output(string.format("Cat failed a read on '%s' (err: %s)", filename, err1))
+      local chunk, err2 = file.read(fd, 1024)
+      if err2 then
+        -- We couldn't read it, lets move onto
+ the next file, something is wrong
+        -- with this one
+        output(string.format("Error: Failed a read on '%s' (err: %s)", filename, err2))
+        break
       else
         -- If we got nothing back, we're at the end of the file, break the loop!
         if  chunk == "" then break end
