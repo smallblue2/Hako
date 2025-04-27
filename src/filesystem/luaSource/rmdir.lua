85d4dd20dc66a5b49adb3ce9289992e5af56598e
ogradc23 <cathal.ogrady23@mail.dcu.ie>
2025-04-28 00:09:19 +0100

    HO-87-rmdir add rmdir util

---
+++ src/filesystem/luaSource/rmdir.lua
@@ -0,0 +1,25 @@
+local function usage()
+  output([[Usage: rmdir DIRECTORY...
+Remove DIRECTORY if it is empty]])
+end
+
+if #process.argv == 1 then
+  usage()
+  process.exit(0)
+end
+
+local dirs = { table.unpack(process.argv, 2) }
+for _, dir in ipairs(dirs) do
+  local _, err = file.stat(dir)
+  if err ~= nil then
+    output(string.format("rmdir: error accessing directory '%s': %s", dir, errors.as_string(err)))
+    goto continue
+  end
+
+  err = file.remove_dir(dir)
+  if err ~= nil then
+    output(string.format("rmdir: error removing directory '%s': %s", dir, errors.as_string(err)))
+  end
+
+  ::continue::
+end
